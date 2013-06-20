c2fed94859776723f0fd970736279de223c7dc1d
Jonatan Alexis Anauati <barakwins@gmail.com>
2013-06-20 12:52:43 -0300

    Basic urlconf must be empty.

--- examples/exampleproject_03/urls.py
+++ examples/exampleproject_03/urls.py
@@ -14,6 +14,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^yourwebapp/', include('yourwebapp.urls',namespace="yourwebapp")),
-
 )
