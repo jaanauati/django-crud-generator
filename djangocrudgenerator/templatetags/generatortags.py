22e4a22f12808ba98e57b80a97316f85b1672968
Jonatan Alexis Anauati <barakwins@gmail.com>
2013-06-21 13:46:50 -0300

    Introduces new tags that makes more comfortable the edition and comprehension of templates. PEP8.

---
+++ djangocrudgenerator/templatetags/generatortags.py
@@ -0,0 +1,44 @@
+# -*- coding: utf8 -*-
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def gen_url_tag(*args, **kwargs):
+    """ Helper Template tag that generates an url tag:
+         {% url "the:url:path" %}
+    """
+    _kwargs = reduce(lambda i, k: i+u'%s="%s" ' % (k, kwargs[k]), kwargs, u'')
+    return u'{%% url "%s" %s %%}' % (u":".join(args), _kwargs)
+
+
+@register.simple_tag
+def start_tag():
+    return u"{%"
+
+
+@register.simple_tag
+def end_tag():
+    return u"%}"
+
+
+@register.simple_tag
+def start_var():
+    return u"{{"
+
+
+@register.simple_tag
+def end_var():
+    return u"}}"
+
+
+@register.simple_tag
+def start_comment():
+    return u"{#"
+
+
+@register.simple_tag
+def end_comment():
+    return u"#}"
